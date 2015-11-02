177b56d6f9a55302c2584c36b66c6a8dbe73af38
clavicula <ghost.phant@gmail.com>
2015-11-02 15:35:49 +0900

    ShBOT拡張

--- wiz/bot/shell.py
+++ wiz/bot/shell.py
@@ -26,12 +26,53 @@ class ShellBOT(IRCBOT):
         
         match_sh = re.search(r'^sh +(.+)$', message)
         if match_sh is not None:
-            self.__on_shell(channel_name, match_sh.group(1))
+            command = match_sh.group(1)
+            if command == 'help':
+                self.__on_help(channel_name)
+            else:
+                self.__on_shell(channel_name, command)
             return
+        
+        match_shctrl = re.search(r'^shctrl +(.+)$', message)
+        if match_shctrl is not None:
+            command = match_shctrl.group(1).lower()
+            if command in { 'c' }:
+                self.__on_control_key(channel_name, command)
+            else:
+                self.__client.privmsg(channel_name, "unsupported key : " + command)
+            return
+        
+        match_shkey = re.search(r'^shkey +(.+)$', message)
+        if match_shkey is not None:
+            command = match_shkey.group(1).lower()
+            if command in { 'q' }:
+                self.__on_key(channel_name, command)
+            else:
+                self.__client.privmsg(channel_name, "unsupported key : " + command)
+            return
+    
+    def __on_control_key(self, channel_name, command):
+        self.__core.expect(r'.*$')  # clear buffer
+        self.__core.sendcontrol(command)
+        self.__show_response(channel_name)
+    
+    def __on_help(self, channel_name):
+        self.__client.privmsg(channel_name, "sh [command] : send shell command (ex. sh ls -l)")
+        self.__client.privmsg(channel_name, "sh : simulate the enter key press")
+        self.__client.privmsg(channel_name, "shctrl [key] : simulate the Ctrl+[key] press (ex. shctrl c)")
+    
+    def __on_key(self, channel_name, command):
+        # it doesn't work well...
+        self.__core.expect(r'.*$')  # clear buffer
+        self.__core.send(command)
+        self.__on_shell(channel_name, '')
     
     def __on_shell(self, channel_name, command):
         self.__core.expect(r'.*$')  # clear buffer
         self.__core.sendline(command)
+        self.__show_response(channel_name)
+    
+    def __show_response(self, channel_name):
         time.sleep(0.1)
         
         self.__core.readline()
@@ -46,4 +87,5 @@ class ShellBOT(IRCBOT):
         for result in result_list:
             if not re.search(r'^\[.*@.*\].+$', result):
                 self.__client.privmsg(channel_name, result)
+    
 
